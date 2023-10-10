3928b37fadc0268f62dc2fac4fe334d3a4e9651d
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2023-10-09 21:21:16 -0400

    test: add temporary failing test
    Github actions is weird

--- frontend/tests/sum.test.ts
+++ frontend/tests/sum.test.ts
@@ -5,4 +5,8 @@ describe('sum module', () => {
   test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
   });
+
+  test('this should fail', () => {
+    expect(sum(1, 2)).toBe(0)
+  })
 });
