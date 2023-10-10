4c743ec4dbe127645d2b3001d9c5a94341f8ad28
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2023-10-09 21:22:25 -0400

    test: remove failing temp frontend test

--- frontend/tests/sum.test.ts
+++ frontend/tests/sum.test.ts
@@ -5,8 +5,4 @@ describe('sum module', () => {
   test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
   });
-
-  test('this should fail', () => {
-    expect(sum(1, 2)).toBe(0)
-  })
 });
