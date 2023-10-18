aedf538124cb33a83293f6377b095481cd076871
tfurey96 <98403865+tfurey96@users.noreply.github.com>
2023-10-17 21:38:31 -0400

    feat: paginate browse page created pagination of the browse page

--- frontend/src/components/BrowsePage.tsx
+++ frontend/src/components/BrowsePage.tsx
@@ -1,42 +1,60 @@
-import React from 'react';
-import { Image, List } from 'antd';
+import React, { useState } from 'react';
+import { Image, List  } from 'antd';
 
+
+
+type PaginationPosition = 'top' | 'bottom' | 'both';
+
+type PaginationAlign = 'start' | 'center' | 'end';
 
 const books = [
-    {
-      isbn: '0156007754'
-    },
-    {
-      isbn: '0765365278'
-    },
-    {
-      isbn: ' 0060883286'
-    },
-    {
-      isbn: '1939905214'
-    },
-    {
-      isbn: '198481785X'
-    }
+  {
+    isbn: '0156007754'
+  },
+  {
+    isbn: '0765365278'
+  },
+  {
+    isbn: ' 0060883286'
+  },
+  {
+    isbn: '1939905214'
+  },
+  {
+    isbn: '198481785X'
+  }
 ];
 
-const BrowsePage: React.FC = () => (
-  <List
-    itemLayout="horizontal"
-    dataSource={books}
-    renderItem={(book, index) => (
-      <List.Item>
-        <List.Item.Meta
-          avatar={<Image 
-             src={`https://covers.openlibrary.org/b/isbn/${book.isbn}-M.jpg`}
-             
-              />}
-          title={<a href="https://ant.design">{book.isbn}</a>}
-          description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-        />
-      </List.Item>
-    )}
-  />
-);
+
+const BrowsePage: React.FC = () => {
+  const [position] = useState<PaginationPosition>('bottom');
+  const [align] = useState<PaginationAlign>('center');
+
+  return (
+    <>
+     
+      <List
+        pagination={{ 
+          position,
+          align,
+          defaultPageSize: 3,
+         }}
+        dataSource={books}
+        renderItem={(book) => (
+          <List.Item>
+            <List.Item.Meta
+              avatar={
+                <Image src={`https://covers.openlibrary.org/b/isbn/${book.isbn}-M.jpg`} />
+              }
+              title={<a href="https://ant.design">{book.isbn}</a>} // change href to book page out of scope
+              description="this is a book"
+            />
+          </List.Item>
+        )}
+      />
+    </>
+  );
+};
 
 export default BrowsePage;
+
