f8ea3299e7f1fcbfcd9f2784b7aae26e412cc2bd
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2023-10-23 12:23:42 -0400

    fix(api): allow `OPTIONS` requests
    Browsers run a preflight request as part of CORS
    
    Refs: https://developer.mozilla.org/en-US/docs/Glossary/Preflight_request

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: 'http://localhost:5173',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true
   })
 );
