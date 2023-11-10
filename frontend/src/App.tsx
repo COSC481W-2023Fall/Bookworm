5fdaa1563c193e8b5af7a78ee6927c1d9ed81d3d
Genyuan Liao <70328499+Leslie48197@users.noreply.github.com>
2023-11-10 00:02:55 -0500

    * update: update App.tsx
    * update: update App.tsx
    * add some routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import { ConfigProvider } from 'antd';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Browse from './pages/Browse';
 import Home from './pages/Home';
 import ProfileLayout from './pages/ProfileLayout';
 import ShowProfile from './pages/ShowProfile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import Search from './pages/Search';
+import BookView from './pages/BookView';
 
 function App(): JSX.Element {
   return (
@@ -21,9 +22,10 @@ function App(): JSX.Element {
         <Route path='/' element={<Home />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
-        <Route path='/browse' element={<Browse />} />
         <Route path='/profile/:username' element={<ShowProfile />} />
         <Route path='/profile' element={<ProfileLayout />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='/book/:isbn' element={<BookView />} />
       </Routes>
     </ConfigProvider>
   );
