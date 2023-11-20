f2ae7a66f7028d1b7f845982eb1d115afe96aab5
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2023-11-20 10:35:45 -0500

    refactor(db): rename file, create `IProfile`

---
+++ backend/src/models/profile.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+export interface IProfile {
+  gender: string,
+  occupation: string,
+  favoriteBook: string,
+  description: string,
+  username: string
+}
+
+const profileSchema = new mongoose.Schema<IProfile>({
+  gender: String,
+  occupation: String,
+  favoriteBook: String,
+  description: String,
+  username: { type: String, unique: true, required: true }
+});
+
+const ProfileModel = mongoose.model('Profile', profileSchema);
+
+export default ProfileModel;
