1e88d2f6d04a51427cb8811835ce55ee1bdb72ae
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2023-11-27 00:55:21 -0500

    feat: add props to `ShowUserAvatar`
    Allow flexibility in username resolution on `ShowUserAvatar` by
    adding a `name` prop.

--- frontend/src/components/ShowUserAvatar.tsx
+++ frontend/src/components/ShowUserAvatar.tsx
@@ -2,10 +2,24 @@ import { Avatar } from 'antd';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
-const ShowUserAvatar: React.FC = () => {
+type UserAvatarProps = {
+  name?: string
+}
+
+const ShowUserAvatar: React.FC<UserAvatarProps> = ({ name }) => {
   // Get the first character of the username
-  // Use the useParams hook to get the username from the route
-  const { username } = useParams<{ username?: string }>();
+  // If no name is provided to the component, use
+  // the useParams hook to get the username from the route.
+  // Else, use the username passed to the component
+  let username: string | undefined;
+  if (!name) {
+    let params = useParams<{ username?: string }>();
+    username = params.username;
+  }
+  else {
+    username = name;
+  }
+  
   const firstCharacter: string | undefined = username
     ? username.charAt(0).toUpperCase()
     : undefined;
