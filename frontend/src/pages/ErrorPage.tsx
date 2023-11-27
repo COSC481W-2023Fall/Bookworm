f4fdd3e90664be230cfa9389602ec2b5e476cdf6
Genyuan Liao <70328499+Leslie48197@users.noreply.github.com>
2023-11-27 13:28:41 -0500

    fix: solve ErrorPage.tsx bugs
    * fix: solve ErrorPage.tsx bugs

--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import './ErrorPage.css';
 
-const ErrorPage: React.FC = () => {
+function ErrorPage() {
   return (
-    <div className="container">
+    <div className='container'>
       <h1>404</h1>
-      <p className="page">Page Not Found!</p>
-      <p>Sorry, but the page you are looking for doesn't exist.</p>
-      <Link to="/" className="goback">Go back home</Link>
+      <p className='page'>Page Not Found!</p>
+      <p>Sorry, but the page you are looking for does not exist.</p>
+      <Link to='/' className='goback'>
+        Go Back Home
+      </Link>
     </div>
   );
-};
+}
 
 export default ErrorPage;
