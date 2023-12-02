5a49f0e2f7fecbfb47be4f5a0aa6c10c110b1a55
Genyuan Liao <70328499+Leslie48197@users.noreply.github.com>
2023-12-01 20:44:29 -0500

    fix: change error page go back home button style
    * fix: change error page go back home button style

--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import './ErrorPage.css';
+import { Button } from 'antd';
 
 function ErrorPage() {
   return (
@@ -7,8 +8,18 @@ function ErrorPage() {
       <h1>404</h1>
       <p id='page'>Page Not Found!</p>
       <p>Sorry, but the page you are looking for does not exist.</p>
-      <Link to='/' id='goback'>
-        Go Back Home
+      <Link to='/'>
+        <Button
+          type='primary'
+          style={{
+            width: '300px',
+            height: '40px',
+            borderRadius: '20px',
+            backgroundColor: 'var(--secondary-button-background)'
+          }}
+        >
+          Go Back Home
+        </Button>
       </Link>
     </div>
   );
